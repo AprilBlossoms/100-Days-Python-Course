db88037132b04ea41b2077218750b7352715341d
April Myles <Aprilmayblossoms@gmail.com>
2021-12-19 04:38:57 -0600

    finished the day 15 project

--- intermediate/15-coffee-machine/main.py
+++ intermediate/15-coffee-machine/main.py
@@ -26,6 +26,50 @@ MENU = {
 
 resources = {
     "water": 300,
-    "milk": 200,
+    "milk": 0,
     "coffee": 100,
+    "money": 0
 }
+
+on = True
+def check_resources(order):
+    for ingredient in MENU[order]['ingredients']:
+        if resources[ingredient] < MENU[order]['ingredients'][ingredient]:
+            print(f"Sorry, there is not enough {ingredient}.")
+            return
+    return True
+
+def check_payment(cost, paid):
+    if paid >= cost:
+        return True
+
+while on:
+    order = input("What would you like? (espresso/latte/cappuccino) ")
+    if order == 'report':
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${resources['money']}")
+    elif order == 'off':
+        on = False
+    else:
+        if check_resources(order):
+            print(f"The cost is ${MENU[order]['cost']}.")
+            quarters = int(input("How many quarters will you use? ")) * .25
+            dimes = int(input("How many dimes? ")) * .1
+            nickels = int(input("How many nickels? ")) * .05
+            pennies = int(input("How many pennies? ")) * .01
+            total_inserted = quarters + dimes + nickels + pennies
+            if check_payment(MENU[order]['cost'], total_inserted):
+                for ingredient in MENU[order]['ingredients']:
+                    resources[ingredient] -= MENU[order]['ingredients'][ingredient]
+                resources['money'] += MENU[order]['cost']
+                change_due = round(total_inserted - MENU[order]['cost'], 2)
+                print(f"Your change is ${change_due}.")
+                print(f"Here is your {order}. Enjoy!")
+            else:
+                print("Not enough money")
+
+        
+
+
